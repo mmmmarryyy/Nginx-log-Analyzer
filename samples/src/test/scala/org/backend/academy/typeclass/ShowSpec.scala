05780c14adfd619c02da9b3fa5984d913ae3421a
Grigorii Berezin <grisha.berezin.1998@mail.ru>
2024-08-21 13:07:25 +0300

    fix format

--- samples/src/test/scala/org/backend/academy/typeclass/ShowSpec.scala
+++ samples/src/test/scala/org/backend/academy/typeclass/ShowSpec.scala
@@ -3,7 +3,7 @@ package org.backend.academy.typeclass
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class ShowSpec extends AnyFlatSpec with Matchers :
+class ShowSpec extends AnyFlatSpec with Matchers:
   private case class Person(firstName: String, lastName: String)
 
   private given Show[Person] with
